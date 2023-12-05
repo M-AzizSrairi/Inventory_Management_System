a70971b6d2b5393ca7dbbdf512948fd75bc22aee
M-AzizSrairi <azizsrairi00@gmail.com>
2023-12-05 11:13:14 +0100

    update person

--- Classes/Person.java
+++ Classes/Person.java
@@ -1,10 +1,31 @@
 package Classes;
 
-public class Person {
+public abstract class Person {
+    private static int nextPersonID = 1000;
     private int personID;
     private String name;
     private int age;
 
+    //Constructor 
+    public Person(String name, int age){
 
+        this.personID = nextPersonID++;
+
+        if (name != null || !name.trim().isEmpty()){
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Name must not be null or empty");
+        }
+
+        if (age >= 16 && age <= 90) {
+            this.age = age;
+        } else {
+            throw new IllegalArgumentException("Age must be within the range (16,90) inclusive ");
+        }
+
+    }
+
+    //abstract method - display Info
+    public abstract void displayInfo();
     
 }
