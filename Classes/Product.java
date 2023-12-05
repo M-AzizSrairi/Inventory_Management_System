6157f6823ef27cac4b66b22c822f24bbe9bc3d18
RayenFares <rayenfares56@gmail.com>
2023-12-05 11:09:46 +0100

    Product Class

---
+++ Classes/Product.java
@@ -0,0 +1,10 @@
+public class Product {
+    private int productId;
+    private String productName;
+    private double buyingPrice;
+    private double sellingPrice;
+    private String category;
+
+    // Constructor, getters, and setters
+}
+
