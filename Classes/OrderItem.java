d5dc3c6feee81b90a53c0e8cf49ebbb062da2afc
M-AzizSrairi <azizsrairi00@gmail.com>
2023-12-05 17:26:35 +0100

    Update OrderItem.java

--- Classes/OrderItem.java
+++ Classes/OrderItem.java
@@ -3,11 +3,13 @@ public class OrderItem {
     private Product product;
     private int quantity;
     private double buyingPrice;
+    private Supplier supplier;
 
-    public OrderItem(Product product, int quantity, double buyingPrice) {
+    public OrderItem(Product product, int quantity, double buyingPrice, Supplier supplier) {
         this.product = product;
         this.quantity = quantity;
         this.buyingPrice = buyingPrice;
+        this.supplier = supplier;
     }
 
     public Product getProduct() {
@@ -24,6 +26,15 @@ public class OrderItem {
 
     public double getOrderItemTotalPrice() {
         return buyingPrice * quantity;
+    }
+
+    public Supplier getSupplier(){
+        return this.supplier;
+    }
+
+    //print invoice
+    public void issueInvoice(){
+        //to be added;
     }
 
 
